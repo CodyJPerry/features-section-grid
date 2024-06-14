67f969d2bc2d9ec47319dc40e68e2b9caf3eb0d8
Cody Perry <perrysdevelopment@gmail.com>
2024-06-13 20:26:26 -0400

    Add icons and features collection

---
+++ app/data/features.tsx
@@ -0,0 +1,43 @@
+import {
+  RiDownload2Line,
+  RiBrush2Line,
+  RiCopyrightLine,
+  RiRefund2Fill,
+  RiTeamLine,
+  RiRefreshLine,
+} from 'react-icons/ri';
+
+const features = [
+  {
+    icon: <RiDownload2Line />,
+    title: 'Infinite Download',
+    description: `Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.`,
+  },
+  {
+    icon: <RiBrush2Line />,
+    title: 'Purely Handcrafted',
+    description: `No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.`,
+  },
+  {
+    icon: <RiCopyrightLine />,
+    title: 'All Are Under licensed',
+    description: `The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).`,
+  },
+  {
+    icon: <RiRefund2Fill />,
+    title: 'Cancel Anytime',
+    description: `Subscribe at your own pace, and cancel when you feel it's enough.`,
+  },
+  {
+    icon: <RiTeamLine />,
+    title: 'Feature 5',
+    description: `We support multiple seats at once, requiring only a single payment.`,
+  },
+  {
+    icon: <RiRefreshLine />,
+    title: 'No Limitations',
+    description: `Use as many as you want, from Dribbble presentations to PowerPoint presentations.`,
+  },
+];
+
+export default features;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+
+// Import our features
+import features from './data/features';
 
 export default function Home() {
+  console.log('featues =>', features);
   return (
-    <main className="bg-white m-4 shadow-sm rounded">
-      <h1>Hello there!</h1>
-    </main> 
+    <main className='bg-white m-4 shadow-sm rounded'>
+      <section>
+        <h1>Easy access to top quality images</h1>
+        <p>Premium abstract images</p>
+        <p>
+          In a world where storytelling constantly evolves, we lead with
+          groundbreaking images designed for your presentation excellence.
+        </p>
+      </section>
+    </main>
   );
 }
