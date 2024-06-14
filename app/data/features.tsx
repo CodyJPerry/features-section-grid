228915300e7769a241dde0d0292ea6b231cc444d
Cody Perry <perrysdevelopment@gmail.com>
2024-06-13 20:44:56 -0400

    Update text styling

--- app/data/features.tsx
+++ app/data/features.tsx
@@ -10,37 +10,37 @@ import {
 const features = [
   {
     id: 1,
-    icon: <RiDownload2Line />,
+    icon: <RiDownload2Line className='h-6 w-6 text-indigo-700' />,
     title: 'Infinite Download',
     description: `Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.`,
   },
   {
     id: 2,
-    icon: <RiBrush2Line />,
+    icon: <RiBrush2Line className='h-6 w-6 text-indigo-700' />,
     title: 'Purely Handcrafted',
     description: `No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.`,
   },
   {
     id: 3,
-    icon: <RiCopyrightLine />,
+    icon: <RiCopyrightLine className='h-6 w-6 text-indigo-700' />,
     title: 'All Are Under licensed',
     description: `The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).`,
   },
   {
     id: 4,
-    icon: <RiRefund2Fill />,
+    icon: <RiRefund2Fill className='h-6 w-6 text-indigo-700' />,
     title: 'Cancel Anytime',
     description: `Subscribe at your own pace, and cancel when you feel it's enough.`,
   },
   {
     id: 5,
-    icon: <RiTeamLine />,
+    icon: <RiTeamLine className='h-6 w-6 text-indigo-700' />,
     title: 'Feature 5',
     description: `We support multiple seats at once, requiring only a single payment.`,
   },
   {
     id: 6,
-    icon: <RiRefreshLine />,
+    icon: <RiRefreshLine className='h-6 w-6 text-indigo-700' />,
     title: 'No Limitations',
     description: `Use as many as you want, from Dribbble presentations to PowerPoint presentations.`,
   },
--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,13 @@ export default function Home() {
   return (
     <main className='bg-white m-4 shadow-sm rounded'>
       <section>
-        <h1>Easy access to top quality images</h1>
-        <p>Premium abstract images</p>
-        <p>
+        <h1 className='text-3xl font-semibold text-neutral-900'>
+          Easy access to top quality images
+        </h1>
+        <p className='text-base font-semibold text-indigo-700'>
+          Premium abstract images
+        </p>
+        <p className='text-base font-normal text-neutral-600'>
           In a world where storytelling constantly evolves, we lead with
           groundbreaking images designed for your presentation excellence.
         </p>
@@ -20,9 +24,13 @@ export default function Home() {
       {/* Map over the features collection to display each feature */}
       {features.map(({ id, icon, title, description }) => (
         <div key={id}>
-          {icon}
-          <p>{title}</p>
-          <p>{description}</p>
+          <span className='h-12 w-12 bg-white rounded-full shadow flex items-center justify-center p-3'>
+            {icon}
+          </span>
+          <p className='text-xl font-semibold text-neutral-900'>{title}</p>
+          <p className='text-base font-normal text-neutral-600'>
+            {description}
+          </p>
         </div>
       ))}
     </main>
