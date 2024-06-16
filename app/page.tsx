d641e80077ecfe42923b932a3714123d57f3c3c3
Cody Perry <perrysdevelopment@gmail.com>
2024-06-16 11:48:22 -0400

    Add sizes for large screens

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import features from './data/features';
 export default function Home() {
   console.log('featues =>', features);
   return (
-    <main className='bg-white m-4 shadow-sm rounded py-12 md:py-16 px-3 md:px-4'>
+    <main className='bg-white m-4 shadow-sm md:shadow-md lg:shadow-lg rounded md:rounded-md py-12 md:py-16 lg:py-24 px-3 md:px-4 lg:px-24 container mx-auto'>
       <section className='grid grid-col-1 text-center mb-12 md:mb-16'>
         <h1 className='text-3xl md:text-5xl font-semibold text-neutral-900 order-2 mb-5'>
           Easy access to top quality images
@@ -22,7 +22,7 @@ export default function Home() {
         </p>
       </section>
       {/* Map over the features collection to display each feature */}
-      <section className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-x-8 lg:gap-y-12'>
         {features.map(({ id, icon, title, description }) => (
           <div key={id} className='flex flex-col items-center text-center'>
             <span className='h-12 w-12 bg-white rounded-full shadow flex items-center justify-center p-3 mb-5'>
